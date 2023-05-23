14775299c3e67f13b139e5531326ca035ba510cd
Cherrysaki <ksaki0402@gmail.com>
2023-05-23 19:44:10 +0900

    0523 camera

--- ShareStudy/RecordViewController.swift
+++ ShareStudy/RecordViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
-
-class RecordViewController: UIViewController {
-
+import AVFoundation
+class RecordViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    @IBOutlet weak var cameraView: UIView! // Storyboard上のUIViewに接続するIBOutlet
+    @IBOutlet weak var captureButton: UIButton! // Storyboard上のUIButtonに接続するIBOutlet
+    
+    private var captureSession: AVCaptureSession!
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    private var photoOutput: AVCapturePhotoOutput!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupCamera()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupCamera() {
+        captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            print("カメラデバイスが取得できません")
+            return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer.videoGravity = .resizeAspectFill
+            videoPreviewLayer.frame = cameraView.bounds
+            cameraView.layer.addSublayer(videoPreviewLayer)
+            
+            photoOutput = AVCapturePhotoOutput()
+            captureSession.addOutput(photoOutput)
+            
+            captureSession.startRunning()
+        } catch {
+            print("カメラのセットアップに失敗しました: \(error.localizedDescription)")
+        }
     }
-    */
-
+    
+    @IBAction func captureButtonTapped(_ sender: UIButton) {
+        let settings = AVCapturePhotoSettings()
+        
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(), let image = UIImage(data: imageData) {
+            // 撮影した写真を利用する
+            // 例: ギャラリーに保存する、別の画面に表示するなど
+        }
+    }
+    
 }
+
+
+
+
+
