4b4f6a9b0e886f0565dc54ca5fd64520799edae3
Cherrysaki <ksaki0402@gmail.com>
2023-08-13 02:07:02 +0900

    0813 ProfileViewの作成　ドキュメントが取れていない

--- ShareStudy/ProfileViewController.swift
+++ ShareStudy/ProfileViewController.swift
@@ -6,13 +6,57 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var userNameLabel:UILabel!
+    @IBOutlet var userIdlabel: UILabel!
+    @IBOutlet var iconImageView: UIImageView!
+    
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        fetchMyProfile()
+    }
+    
+   
 
+    func fetchMyProfile(){
+        // ログインしているユーザーのUIDを取得
+        if let currentUserID = Auth.auth().currentUser?.uid {
+            // ユーザーのドキュメント参照を作成
+            let userDocRef = db.collection("user").document(currentUserID)
+            
+            // ユーザーのデータを取得
+            userDocRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    // ドキュメントが存在する場合、データを取得
+                    if let data = document.data(),
+                       let userName = data["userName"] as? String,
+                       let userId = data["userId"] as? String,
+                       let iconImageURL = URL(string: data["profileImageName"] as! String){
+                        print("名前: \(userName)")
+                        print("id: \(userId)")
+                        
+                        self.userNameLabel.text = userName
+                        self.userIdlabel.text = userId
+                        
+                        let iconData = NSData(contentsOf: iconImageURL)
+                        let iconImage = UIImage(data: iconData! as Data)!
+                        self.iconImageView.image = iconImage
+                        
+                    }
+                } else {
+                    print("ユーザーのドキュメントが存在しないかエラーが発生しました")
+                }
+            }
+        }
+        
+    }
 
 }
--- ShareStudy/SettingViewController.swift
+++ ShareStudy/SettingViewController.swift
@@ -10,6 +10,7 @@ import Firebase
 
 class SettingViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +18,6 @@ class SettingViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
